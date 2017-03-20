a7c170ed5b0696f489943854339ea28fd7b35b79
Goodzc <1214609356@qq.com>
2017-03-20 20:46:31 +0800

    hhhh

---
+++ views/script/01/js.js
@@ -0,0 +1,47 @@
+
+var time=10;
+if(time<20){
+    console.log("Good day");
+
+}
+else{
+    console.log("Good evening");
+}
+/*1*/
+var shijian=30;
+if(shijian<10){
+    console.log("Good")
+}
+else if(shijian>=10&&shijian<20){
+    console.log(" ok")
+}
+else{
+    console.log(" no")
+}
+var num=1;
+if(num===1){
+    console.log("num is equal to 1");
+}
+var number=0;
+if(num===1){
+    console.log("num is equal to 1");
+}
+else {
+    console.log("num is not equal to 1,the value of num is "+number);
+}
+var month=5;
+if(month===1){
+    console.log("January");
+}
+else if(month===2){
+    console.log("February");
+}
+else if (month===3){
+    console.log("march");
+}
+else {
+    console.log("Month is not January,Fe")
+}
+/**
+ * Created by ASUS on 2017/3/20.
+ */
